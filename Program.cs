0f1e2753bf7722bb940ae5f19c142fb98b8d93f0
Ryan Lirio <ryanlirio2@gmail.com>
2024-09-17 14:00:38 -0300

    usandoOdowhile

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        uint i = 0;
+        do
+        {
+            Console.WriteLine(i);
+            i-- ;
+        }while (i < 5);
+    }
+}
